16ea3a0d2917456cfc789ddf05387d4191429adb
Marcin Jaborski <marcin.jaborski@gmail.com>
2025-07-14 15:48:47 +0200

    Fix macro table rounding and layout

--- src/components/molecules/MacroTable/MacroTable.tsx
+++ src/components/molecules/MacroTable/MacroTable.tsx
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material";
 import { Grid } from "@mui/system";
+import _ from "lodash";
 import { useTranslation } from "react-i18next";
 
 type MacroTableProps = {
@@ -13,18 +14,18 @@ function MacroTable({ calories, proteins, fats, carbohydrates }: MacroTableProps
   const { t } = useTranslation("MacroTable");
 
   return (
-    <Grid container spacing={2} sx={{ textAlign: "center" }}>
+    <Grid container spacing={2} sx={{ textAlign: "center", whiteSpace: "nowrap" }}>
       <Grid size={3}>
-        <Typography>{t("calories", { calories })}</Typography>
+        <Typography>{t("calories", { calories: _.round(calories, 1) })}</Typography>
       </Grid>
       <Grid size={3}>
-        <Typography>{t("proteins", { proteins })}</Typography>
+        <Typography>{t("proteins", { proteins: _.round(proteins, 1) })}</Typography>
       </Grid>
       <Grid size={3}>
-        <Typography>{t("fats", { fats })}</Typography>
+        <Typography>{t("fats", { fats: _.round(fats, 1) })}</Typography>
       </Grid>
       <Grid size={3}>
-        <Typography>{t("carbohydrates", { carbohydrates })}</Typography>
+        <Typography>{t("carbohydrates", { carbohydrates: _.round(carbohydrates, 1) })}</Typography>
       </Grid>
     </Grid>
   );
